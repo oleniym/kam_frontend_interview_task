0e6e99d37060579a4bba170174eaeae81a7f22a6
Olga Kazeka <okazeka@kameleoon.com>
2023-12-24 14:28:35 +0300

    create all sites

--- kameleoon-backoffice/src/App.js
+++ kameleoon-backoffice/src/App.js
@@ -2,33 +2,6 @@ import React, { useEffect, useState } from 'react';
 import { Navigation, Table } from './components/Markup';
 
 function App() {
-  const fetchData = async (url, setData) => {
-    try {
-      const response = await fetch(url);
-      if (!response.ok) {
-        throw new Error(`Ошибка HTTP: ${response.status}`);
-      }
-      const data = await response.json();
-      setData(data);
-    } catch (error) {
-      console.error('Ошибка при запросе к серверу:', error);
-    }
-  };
-
-    const [sitesData, setSitesData] = useState('');
-    const [testsData, setTestsData] = useState('');
-  
-    useEffect(() => {
-      fetchData('http://localhost:3100/tests', setSitesData);
-    }, []);
-  
-    useEffect(() => {
-      fetchData('http://localhost:3100/sites', setTestsData);
-    }, []);
-  
-    console.log('{sitesData}', { sitesData });
-    console.log('{testsData}', { testsData });
-
   return (
     <div className="dashboard">
       <div className="dashboard__header">
