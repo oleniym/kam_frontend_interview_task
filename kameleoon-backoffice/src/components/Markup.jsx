f3df33a54c6e1b33718da3cb7f970fe1cc739daa
Olga Kazeka <okazeka@kameleoon.com>
2023-12-24 18:41:10 +0300

    create search

--- kameleoon-backoffice/src/components/Markup.jsx
+++ kameleoon-backoffice/src/components/Markup.jsx
@@ -1,7 +1,5 @@
 import React, { useEffect, useState } from 'react';
 
-
-
 const fetchData = async (url) => {
   try {
     const response = await fetch(url);
@@ -17,7 +15,7 @@ const fetchData = async (url) => {
   }
 };
 
-const fetchDataForBoth = async () => {
+const getData = async () => {
   try {
     const [sitesData, testsData] = await Promise.all([
       fetchData('http://localhost:3100/sites'),
@@ -63,7 +61,7 @@ const TestTableItem = ({ test, site }) => {
   );
 };
 
-const TestTable = ({ testsData, sitesData, searchQuery }) => {
+const TestTable = ({ testsData, sitesData, searchQuery, onResetSearch }) => {
   const siteLookup = {};
   sitesData.forEach((site) => {
     siteLookup[site.id] = site;
@@ -73,23 +71,45 @@ const TestTable = ({ testsData, sitesData, searchQuery }) => {
     test.name.toLowerCase().includes(searchQuery.toLowerCase())
   );
 
+  const noResults = filteredTests.length === 0;
+
+  const handleResetSearch = () => {
+    onResetSearch();
+
+    const searchInput = document.querySelector('.searchbar__field-text');
+    if (searchInput) {
+      searchInput.value = '';
+    }
+  };
+
+  if (testsData.length === 0 || sitesData.length === 0) {
+     return null;
+  }
+
   return (
     <div className="table">
-      {filteredTests.map((test) => {
-        const site = siteLookup[test.siteId];
-        return <TestTableItem key={test.id} test={test} site={site} />;
-      })}
+      {noResults ? (
+        <div className="no-results">
+          <p>Your search did not match any results</p>
+          <button onClick={handleResetSearch}>Reset</button>
+        </div>
+      ) : (
+        filteredTests.map((test) => {
+          const site = siteLookup[test.siteId];
+          return <TestTableItem key={test.id} test={test} site={site} />;
+        })
+      )}
     </div>
   );
 };
 
-export const Navigation = ({ onSearch }) => (
+const Navigation = ({ onSearch, numTests, showNavigationInfo }) => (
   <div className="navigation">
     <div className="navigation__search">
       <div className="searchbar">
         <div className="searchbar__field">
           <svg xmlns="http://www.w3.org/2000/svg" width="13" height="14" viewBox="0 0 13 14" fill="none">
-          <path d="M12.802 12.751L9.59779 9.28668C10.4217 8.26764 10.8735 6.98629 10.8735 5.65218C10.8735 2.53599 8.43501 0 5.43575 0C2.43851 0 0 2.53529 0 5.65218C0 8.76978 2.43851 11.3051 5.43575 11.3051C6.56132 11.3051 7.6348 10.9527 8.55068 10.2818L11.7799 13.7735C11.9152 13.9198 12.0958 14 12.2913 14C12.4746 14 12.6491 13.9269 12.7817 13.7939C13.0651 13.5119 13.0733 13.045 12.802 12.751ZM5.43575 1.47476C7.65171 1.47476 9.45506 3.34897 9.45506 5.65218C9.45506 7.95609 7.65239 9.83031 5.43575 9.83031C3.22113 9.83031 1.41846 7.95609 1.41846 5.65218C1.41846 3.34968 3.22113 1.47476 5.43575 1.47476Z" fill="#222222"/>
+            <path d="M12.802 12.751L9.59779 9.28668C10.4217 8.26764 10.8735 6.98629 10.8735 5.65218C10.8735 2.53599 8.43501 0 5.43575 0C2.43851 0 0 2.53529 0 5.65218C0 8.76978 2.43851 11.3051 5.43575 11.3051C6.56132 11.3051 7.6348 10.9527 8.55068 10.2818L11.7799 13.7735C11.9152 13.9198 12.0958 14 12.2913 14C12.4746 14 12.6491 13.9269 12.7817 13.7939C13.0651 13.5119 13.0733 13.045 12.802 12.751ZM5.43575 1.47476C7.65171 1.47476 9.45506 3.34897 9.45506 5.65218C9.45506 7.95609 7.65239 9.83031 5.43575 9.83031C3.22113 9.83031 1.41846 7.95609 1.41846 5.65218C1.41846 3.34968 3.22113 1.47476 5.43575 1.47476Z" fill="#222222"/>
           </svg>
           <input
             autoComplete="off"
@@ -99,18 +119,20 @@ export const Navigation = ({ onSearch }) => (
             type="text"
             onChange={(e) => onSearch(e.target.value)}>
           </input>
-          <button className="searchbar__field-info" type="button">7 tests</button>
+          <button className="searchbar__field-info" type="button">{numTests} tests</button>
         </div>
       </div>
     </div>
-    <div className="navigation__info">
-      <div className="textfield">
-        <div className="textfield__item" about="name">Name</div>
-        <div className="textfield__item" about="type">Type</div>
-        <div className="textfield__item" about="status">Status</div>
-        <div className="textfield__item" about="site">Site</div>
+    {showNavigationInfo && (
+      <div className="navigation__info">
+        <div className="textfield">
+          <div className="textfield__item" about="name">Name</div>
+          <div className="textfield__item" about="type">Type</div>
+          <div className="textfield__item" about="status">Status</div>
+          <div className="textfield__item" about="site">Site</div>
+        </div>
       </div>
-    </div>
+    )}
   </div>
 );
 
@@ -118,23 +140,36 @@ export const Table = () => {
   const [testsData, setTestsData] = useState([]);
   const [sitesData, setSitesData] = useState([]);
   const [searchQuery, setSearchQuery] = useState('');
+  const [numTests, setNumTests] = useState(0);
 
   useEffect(() => {
-    fetchDataForBoth().then(([sites, tests]) => {
+    getData().then(([sites, tests]) => {
       setSitesData(sites);
       setTestsData(tests);
+      setNumTests(tests.length);
     });
   }, []);
 
   const handleSearch = (query) => {
     setSearchQuery(query);
+    const filteredTests = testsData.filter((test) =>
+      test.name.toLowerCase().includes(query.toLowerCase())
+    );
+    setNumTests(filteredTests.length);
   };
+
+  const handleResetSearch = () => {
+    setSearchQuery('');
+    setNumTests(testsData.length);
+  };
+
+  const showNavigationInfo = numTests > 0;
 
   return (
     <div className="table">
       <div className="table__wrapper">
-        <Navigation onSearch={handleSearch} />
-        <TestTable testsData={testsData} sitesData={sitesData} searchQuery={searchQuery} />
+        <Navigation onSearch={handleSearch} numTests={numTests} showNavigationInfo={showNavigationInfo} />
+        <TestTable testsData={testsData} sitesData={sitesData} searchQuery={searchQuery} onResetSearch={handleResetSearch} />
       </div>
     </div>
   );
